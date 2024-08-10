7d701abb6c41e57e887a99bfbb12ca496b3b485d
mustafaduyarer <mustafaduyarer@gmail.com>
2024-08-11 00:44:50 +0200

    set up redux forum in searchbar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class SearchBar extends Component {
-  render() {
+
+  handleFormSubmit = function ({ query }) {
+    console.log("trying to handle subimit for query", query);
+    //navigate to a new route.
+  };
+
+  renderInput(field) {
     return (
-      <form >
-<input placeholder="Search DailySmarty"/>
+      <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <Field name="query" component={this.renderInput} />
       </form>
     );
   }
 }
+
+SearchBar = reduxForm({
+  form: "searchBar",
+})(SearchBar);
+
 export default SearchBar;
